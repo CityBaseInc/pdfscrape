a9d1b02f0ec9b9a8bec86bad0cac2e983eb994c7
Vidal Anguiano <vidal.anguiano@gmail.com>
2018-11-26 14:29:21 -0600

    Cleans repository

--- pdfscrape_runm.py
+++
@@ -1,9 +0,0 @@
-from multiprocessing import Pool
-from runt import runt
-
-if __name__ == "__main__":
-    p = Pool(2)
-    l = ['pdfs_small1', 'pdfs_small2']
-    p.map(runt, l)
-    p.terminate()
-    p.join()
--- runt.py
+++
@@ -1,6 +0,0 @@
-from pdfscrape.pdf_pipeline import PDFPipeline
-
-def runt(table):
-    # l = ["*{maxpages=10, base=5, random_sample_size=5,sep='`'}"]
-    pdfp = PDFPipeline(table)
-    pdfp.scrape_pdfs(10, 5, 5, sep='`', multi=True)
